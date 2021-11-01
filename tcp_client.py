5417e89a024d0796c2a9badd5f59cea320e339cf
Kasie Madden <Kasie.madden@gmail.com>
2021-11-01 18:17:46 -0400

    adding lab 7 for Networworkd. Creating a chat box

---
+++ tcp_client.py
@@ -0,0 +1,12 @@
+import socket
+target_host = "0.0.0.0"
+target_port = 27700
+# create a socket connection
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# let the client connect
+client.connect((target_host, target_port))
+# send some data
+client.send("SYN")
+# get some data
+response = client.recv(4096)
+print (response)
