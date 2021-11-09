b6b4632364805ee06857e115cc92e124f6f0f3d4
Kasie Madden <Kasie.madden@gmail.com>
2021-11-09 17:18:33 -0500

    Working Code

--- UDPClient.py
+++ UDPClient.py
@@ -1,45 +1,43 @@
 import socket 
 import threading
 import os 
+import sys
 
-target_host  = '127.0.0.1'
-target_port = 12000
+cleintIP  = "127.0.0.1"
+clientPort = 1234
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket.bind((cleintIP, clientPort))
+
+print("\n[*] Starting Chat with client code...%s:%d" % (cleintIP,clientPort))
+print("The chat is ready.\n\n")
+
+#os.system("tput setaf 2")
+name = input("Enter Your name: ")
+if(name == "QUIT"):
+    os._exit(1)
+
+print("Type *QUIT* to exit the program. \n\n")
 
 
 def send():
     while True:
-        outMess = input("Me: ")
-        if (outMess == "quit" | outMess == "Quit" | outMess == "QUIT"):
+        
+        outMess = input()
+        if (outMess == "QUIT"):
             os._exit(1)
 
-        clientSocket.send(str(outMess).encode())
-        print("\n")
+        sendMess = "{}>> {}".format(name,outMess)
+        clientSocket.sendto(sendMess.encode(),("127.0.0.1",4321) ) #server IP and port
+
 
 def receive():
     while True: 
         inMess = clientSocket.recvfrom(2048)
-        print("Them: " + inMess[0].decode())
-        
-
-        print("\n")
+        print("\t\t\t\t" + inMess[0].decode()) 
 
 #creating threads 
 T1 = threading.Thread(target=send)
 T2 = threading.Thread(target=receive)
-
 #starting threads 
 T1.start()
 T2.start()
-
-
-
-
-
-
-
-'''clientSocket.sendto(message.encode(),(target_host, target_port))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
-'''
--- UDPSever.py
+++ UDPSever.py
@@ -1,50 +1,48 @@
 import socket
 import threading
 import os
-
-
-
-serverIP ='127.0.0.1'
-serverPort = 12000
+import sys
 #no listen 
 #ideally gonna need two threads 
 #make sure to 
 
+
+serverIP ="127.0.0.1"
+serverPort = 4321
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind((serverIP, serverPort))
-print("[*] Starting server...%s:%d" % (serverIP,serverPort) + "\n")
-print("The server is ready. ")
+
+
+print("\n[*] Starting Chat with server code...%s:%d" % (serverIP,serverPort))
+print("The chat is ready.\n\n")
+
+#os.system("tput setaf 2")
+name = input("Enter Your name:")
+if(name == "QUIT"):
+    os._exit(1)
+print("Type *QUIT* to exit the program. \n\n")
+
+
 
 def send():
     while True:
-        outMess = input("Me: ")
-        if (outMess == "quit" | outMess == "Quit" | outMess == "QUIT"):
+        outMess =  input() 
+    
+        if (outMess == "QUIT"):
             os._exit(1)
-
-        serverSocket.send(str(outMess).encode())
-        print("\n")
+        
+        sendMess = "{}>> {}".format(name,outMess)
+        serverSocket.sendto(sendMess.encode(),("127.0.0.1",1234) ) #client IP and port
 
 def receive():
     while True: 
-        inMess = serverSocket.recvfrom(2048)
-        print("Them: " + inMess[0].decode())
+        inMess = serverSocket.recvfrom(2048)     
+        print("\t\t\t\t" +  inMess[0].decode())
         
-
-        print("\n")
-
 #creating threads 
 T1 = threading.Thread(target=send)
 T2 = threading.Thread(target=receive)
-
 #starting threads 
 T1.start()
 T2.start()
 
-
-'''while True:
-    #no accept
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(),clientAddress)
-    print ("[*] Client Socket Address: %s Send string to the client: %s" % (clientAddress, modifiedMessage)) 
-'''
