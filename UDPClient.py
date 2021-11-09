c9bad7faf11b308d5f2c73f73c58235c7ada5ee6
Kasie Madden <Kasie.madden@gmail.com>
2021-11-09 14:54:29 -0500

    added threads to and send rec ()

--- UDPClient.py
+++ UDPClient.py
@@ -1,9 +1,45 @@
-from socket import *
-serverName = '127.0.0.1'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence: ')
-clientSocket.sendto(message.encode(),(serverName, serverPort))
+import socket 
+import threading
+import os 
+
+target_host  = '127.0.0.1'
+target_port = 12000
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def send():
+    while True:
+        outMess = input("Me: ")
+        if (outMess == "quit" | outMess == "Quit" | outMess == "QUIT"):
+            os._exit(1)
+
+        clientSocket.send(str(outMess).encode())
+        print("\n")
+
+def receive():
+    while True: 
+        inMess = clientSocket.recvfrom(2048)
+        print("Them: " + inMess[0].decode())
+        
+
+        print("\n")
+
+#creating threads 
+T1 = threading.Thread(target=send)
+T2 = threading.Thread(target=receive)
+
+#starting threads 
+T1.start()
+T2.start()
+
+
+
+
+
+
+
+'''clientSocket.sendto(message.encode(),(target_host, target_port))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
+'''
--- UDPSever.py
+++ UDPSever.py
@@ -1,10 +1,50 @@
-from socket import *
+import socket
+import threading
+import os
+
+
+
+serverIP ='127.0.0.1'
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('127.0.0.1', serverPort))
-print("The server is ready to receive")
-while True:
+#no listen 
+#ideally gonna need two threads 
+#make sure to 
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind((serverIP, serverPort))
+print("[*] Starting server...%s:%d" % (serverIP,serverPort) + "\n")
+print("The server is ready. ")
+
+def send():
+    while True:
+        outMess = input("Me: ")
+        if (outMess == "quit" | outMess == "Quit" | outMess == "QUIT"):
+            os._exit(1)
+
+        serverSocket.send(str(outMess).encode())
+        print("\n")
+
+def receive():
+    while True: 
+        inMess = serverSocket.recvfrom(2048)
+        print("Them: " + inMess[0].decode())
+        
+
+        print("\n")
+
+#creating threads 
+T1 = threading.Thread(target=send)
+T2 = threading.Thread(target=receive)
+
+#starting threads 
+T1.start()
+T2.start()
+
+
+'''while True:
+    #no accept
     message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(),clientAddress)
     print ("[*] Client Socket Address: %s Send string to the client: %s" % (clientAddress, modifiedMessage)) 
+'''
